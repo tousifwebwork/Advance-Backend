f35231ccb9b9a45077a3b9e5ad9a2b217a91edb5
tousifwebwork <tousifwebwork@gmail.com>
2026-09-25 10:56:43 +0530

    AI integration

---
+++ level4/app.js
@@ -0,0 +1,60 @@
+require('dotenv').config();
+
+const express = require('express');
+const { GoogleGenAI } = require('@google/genai');
+
+const app = express();
+
+const ai = new GoogleGenAI({
+  apiKey: process.env.GEMINI_API_KEY
+});
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.post('/generate', async (req, res) => {
+  const { prompt } = req.body;
+
+  try {
+    const response = await ai.models.generateContent({
+      model: 'gemini-2.5-flash',
+
+      contents: [
+        {
+          role: 'user',
+          parts: [
+            {
+              text: `Explain in one line in short: ${prompt}`
+            }
+          ]
+        }
+      ],
+
+      config: {
+        systemInstruction:
+          'You are an AI assistant and your name is Jaris. If you are unable/ dont know how to answer a question, please respond with "I am not sure about that."',
+      }
+    });
+
+    res.json({
+      text: response.text
+    });
+
+  } catch (error) {
+    console.error('Gemini Error:', error);
+
+    res.status(500).json({
+      error: 'An error occurred while generating content.'
+    });
+  }
+});
+
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
