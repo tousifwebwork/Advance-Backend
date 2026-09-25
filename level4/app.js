8d4730bce6a19be5cdfeba3b6066a0d7731c3b52
tousifwebwork <tousifwebwork@gmail.com>
2026-09-25 15:21:54 +0530

    AI Configuration

--- level4/app.js
+++ level4/app.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 
 const express = require('express');
 const { GoogleGenAI } = require('@google/genai');
+const { ChatGoogleGenerativeAI } = require("@langchain/google-genai");
+
 
 const app = express();
 
@@ -12,42 +14,75 @@ const ai = new GoogleGenAI({
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+
+
+//Without Langchain
+// app.post('/generate', async (req, res) => {
+//   const { prompt } = req.body;
+
+//   try {
+//     const response = await ai.models.generateContent({
+//       model: 'gemini-2.5-flash',
+
+//       contents: [
+//         {
+//           role: 'user',
+//           parts: [
+//             {
+//               text: `Explain in one line in short: ${prompt}`
+//             }
+//           ]
+//         }
+//       ],
+
+//       config: {
+//         systemInstruction:
+//           'You are an AI assistant and your name is Jaris. If you are unable/ dont know how to answer a question, please respond with "I am not sure about that."',
+//       }
+//     });
+
+//     res.json({
+//       text: response.text
+//     });
+
+//   } catch (error) {
+//     console.error('Gemini Error:', error);
+
+//     res.status(500).json({
+//       error: 'An error occurred while generating content.'
+//     });
+//   }
+// });
+
+
+
+
+
+
+//With Langchain
+const llm = new ChatGoogleGenerativeAI({
+    model: "gemini-2.5-flash",
+    temperature: 0.5, 
+})
+
 app.post('/generate', async (req, res) => {
-  const { prompt } = req.body;
-
-  try {
-    const response = await ai.models.generateContent({
-      model: 'gemini-2.5-flash',
-
-      contents: [
+    const { prompt } = req.body;
+    const response = await llm.invoke([
         {
-          role: 'user',
-          parts: [
-            {
-              text: `Explain in one line in short: ${prompt}`
-            }
-          ]
+            role: "system",
+            content: 'You are an AI assistant and your name is Jaris. If you are unable/ dont know how to answer a question, please respond with "I am not sure about that."'
+        },
+        {
+            role: "human",
+            content: prompt
         }
-      ],
+    ]);
+    res.json({text: response.content})
+})
 
-      config: {
-        systemInstruction:
-          'You are an AI assistant and your name is Jaris. If you are unable/ dont know how to answer a question, please respond with "I am not sure about that."',
-      }
-    });
 
-    res.json({
-      text: response.text
-    });
 
-  } catch (error) {
-    console.error('Gemini Error:', error);
 
-    res.status(500).json({
-      error: 'An error occurred while generating content.'
-    });
-  }
-});
 
 app.get('/', (req, res) => {
   res.send('Hello, World!');
